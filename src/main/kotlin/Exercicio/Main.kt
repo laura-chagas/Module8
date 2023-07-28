ab176696245fef2a174d02a9b3bd3d200e473c85
laura.chagas <laura.chagas@zup.com.br>
2023-07-28 11:10:05 -0300

    Update Main.kt by Aula67

--- src/main/kotlin/Exercicio/Main.kt
+++ src/main/kotlin/Exercicio/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     val compra = Compra()
 
     //ETAPA 1
-/*
+
     carrinho.add(Laranja("Laranja", 0.25))
     carrinho.add(Maca("Maçã", 0.60))
     carrinho.add(Maca("Maçã", 0.60))
@@ -16,21 +16,20 @@ fun main() {
     }
 
     println("TOTAL: R$%.2f".format(compra.calcularTotal(carrinho)))
-    */
 
     // ETAPA 2
 
-/*    carrinho.clear()
+    carrinho.clear()
     println(compra.calcularTotal(compra.comprarComOfertas(2, carrinho)))
     println(compra.calcularTotal(compra.comprarComOfertas(1, carrinho)))
     println(compra.calcularTotal(compra.comprarComOfertas(0, carrinho)))
-*/
+
 
     //ETAPA 3
- /*   compra.notificarCliente(carrinho)
+   compra.notificarCliente(carrinho)
     carrinho.clear()
     compra.notificarCliente(carrinho)
-*/
+
     //ETAPA 4
     println("FRUTAS DISPONÍVEIS: \n[1] Maça: 3      [2] Laranja: 5")
     println("Informe o código da fruta para comprar e a quantidade que deseja: ")
